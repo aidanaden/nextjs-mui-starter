df24f2d47fcd442f3332d066b8156b08d4c74419
aidanaden <aidanaden@hotmail.com>
2022-04-15 21:37:26 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Button from "@mui/material/Button";
+
+const Home: NextPage = () => {
+  return <Button variant="contained">Hello world</Button>;
+};
+
+export default Home;
